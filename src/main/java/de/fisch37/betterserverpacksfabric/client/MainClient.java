e8ec817de82b09396a4294522d0e3fcdcccf35f7
Fisch37 <seracin4@gmail.com>
2024-09-08 18:05:20 +0200

    Added owo-lib and client environment

---
+++ src/main/java/de/fisch37/betterserverpacksfabric/client/MainClient.java
@@ -0,0 +1,10 @@
+package de.fisch37.betterserverpacksfabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
