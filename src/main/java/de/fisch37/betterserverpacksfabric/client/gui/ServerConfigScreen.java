d0b199bffa10dd404b8030ca277425992cca34d9
Fisch37 <seracin4@gmail.com>
2024-09-10 08:39:03 +0200

    Updated config screen

--- src/main/java/de/fisch37/betterserverpacksfabric/client/gui/ServerConfigScreen.java
+++ src/main/java/de/fisch37/betterserverpacksfabric/client/gui/ServerConfigScreen.java
@@ -1,8 +1,10 @@
 package de.fisch37.betterserverpacksfabric.client.gui;
 
+import de.fisch37.betterserverpacksfabric.client.ToastHelper;
 import de.fisch37.betterserverpacksfabric.client.gui.components.RichTextEditor;
 import de.fisch37.betterserverpacksfabric.networking.PackState;
 import io.wispforest.owo.ui.base.BaseOwoScreen;
+import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
@@ -35,14 +37,36 @@ public class ServerConfigScreen extends BaseOwoScreen<FlowLayout> {
         rootComponent.surface(Surface.VANILLA_TRANSLUCENT);
 
         rootComponent.horizontalAlignment(HorizontalAlignment.CENTER);
-        rootComponent.padding(Insets.top(10));
+        rootComponent.padding(Insets.of(30, 0, 20, 20));
 
-        rootComponent.child(
-                Components.textBox(Sizing.fill(), state.url())
-        ).child(Containers.horizontalFlow(Sizing.fill(), Sizing.content())
-                .child(Components.smallCheckbox(Text.translatable("bsp.config_ui.required")))
-                .child(Components.smallCheckbox(Text.translatable("bsp.config_ui.reshash")))
-        ).child(new RichTextEditor(Sizing.fill(), Sizing.content())
+        rootComponent.child(Containers.verticalFlow(Sizing.fill(), Sizing.content())
+                .child(
+                        Components.textBox(Sizing.fill(), state.url())
+                                .margins(Insets.bottom(20))
+                ).child(Containers.grid(Sizing.fill(), Sizing.content(), 1, 2)
+                        .child(Components.checkbox(Text.translatable("bsp.config_ui.required")), 0, 0)
+                        .child(Components.checkbox(Text.translatable("bsp.config_ui.reshash")), 0, 1)
+                ).child(new RichTextEditor(Sizing.fill(), Sizing.content()))
+        ).child(Containers.verticalFlow(Sizing.content(), Sizing.expand())
+                .child(Containers.horizontalFlow(Sizing.fill(), Sizing.content())
+                        .child(Components.button(Text.translatable("bsp.config_ui.save"), this::save)
+                                .margins(Insets.horizontal(5))
+                        )
+                        .child(Components.button(Text.translatable("bsp.config_ui.cancel"), b -> close()))
+                        .horizontalAlignment(HorizontalAlignment.RIGHT)
+                        .id("button-row")
+                ).verticalAlignment(VerticalAlignment.BOTTOM)
+                .margins(Insets.bottom(10))
+                .id("vertical-buffer")
         );
+    }
+
+    private void save(ButtonComponent saveButton) {
+        ToastHelper.showToast(
+                client,
+                Text.literal("Saved!"),
+                Text.literal("This is a debug message to show that someone pressed the save button!")
+        );
+        close();
     }
 }
